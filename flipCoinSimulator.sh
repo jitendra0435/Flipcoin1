876f05a4d05f29501ee840a9befdfe697522653f
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-12 13:27:05 +0530

    Add calculating paercentage feature

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,23 @@
 #!/bin/bash -x
    head=1
    tail=0
+  count=0
+  count2=0
+  total=10
+  
+  for ((i=1;i<11;i++))
+   do
    per=$((RANDOM%2))
     if [ $per -eq  $head ]
-      then
-        echo "Head"
-      else
-        echo "tail"
+       then
+       head[count1++]=$head
+       else 
+       tail[count2++]=$tail
+      
    fi
-
-
+  done
+  headpercentage=$(($count1*100/10))
+  echo $headpercentage
+  tailpercentage=$(($count2*100/10))
+  echo $tailpercentage
+  
